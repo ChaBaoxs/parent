496d85809c281b54b429e16c19f330a501fc6e4f
jc <1252458721@qq.com>
2023-07-03 11:46:09 +0800

    chabao:knife4j配置 通过knife4j使用swagger2

--- service-oa/src/main/java/com/chabao/auth/controller/SysRoleController.java
+++ service-oa/src/main/java/com/chabao/auth/controller/SysRoleController.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.chabao.auth.service.SysRoleService;
 import com.chabao.model.system.SysRole;
 import com.chabao.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(tags = "角色接口管理")
 @RestController
 @RequestMapping("/admin/system/sysRole")
 public class SysRoleController {
@@ -19,6 +22,7 @@ public class SysRoleController {
     SysRoleService sysRoleService;
 
     //查询所有用户
+    @ApiOperation("查询所有角色")
     @GetMapping("/findAll")
     public Result findAll(){
         List<SysRole> list = sysRoleService.list();
